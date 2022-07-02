47bbdf576e170e930f5090f4a63e451937d8bc6b
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2022-07-02 16:16:06 -0700

    Adding customers database

---
+++ Bio-Rad-Marketing-Contacts/SQL/Customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Customers
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL, 
+    [Company] NCHAR(50) NOT NULL, 
+    [PhoneNumber] NCHAR(12) NOT NULL, 
+    [Address] NCHAR(100) NOT NULL, 
+    [Note] NTEXT NULL, 
+    [CreatedOn] DATETIME NOT NULL
+)
